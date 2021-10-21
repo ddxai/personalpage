578d1f27ff70ac4de695a14929768c08ef9f371f
Maxim Shurkhovetskiy <smu580dd@gmail.com>
2021-10-21 21:37:15 +0300

    Create base views and routes

--- website/core/urls.py
+++ website/core/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='About'),
+    path('contacts/', views.contacts, name='Contacts'),
+    path('category/<str:category_name>/', views.category, name='Category'),
 ]
--- website/core/views.py
+++ website/core/views.py
@@ -2,4 +2,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Homepage")
+
+
+def about(request):
+    return HttpResponse("About")
+
+
+def contacts(request):
+    return HttpResponse("Contacts")
+
+
+def category(request, category_name):
+    return HttpResponse("Category: %s" % category_name)
