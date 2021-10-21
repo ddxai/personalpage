bb4329695ce815c51a1c2aff2645de59578f52e4
Maxim Shurkhovetskiy <smu580dd@gmail.com>
2021-10-21 22:30:05 +0300

    Create models Create model for categories with name and folder_path fields. Create model for pictures with title, description, file_path fields and foreign key linked to category.

--- website/core/models.py
+++ website/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    folder_path = models.CharField(max_length=200)
+
+
+class Picture(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.CharField(max_length=200)
+    file_path = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
