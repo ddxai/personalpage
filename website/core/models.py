234721fe27f0b947bd444fa9d7d5f9a540364792
Maxim Shurkhovetskiy <smu580dd@gmail.com>
2021-10-21 22:41:48 +0300

    Make pictures description nullable

--- website/core/models.py
+++ website/core/models.py
@@ -8,6 +8,6 @@ class Category(models.Model):
 
 class Picture(models.Model):
     title = models.CharField(max_length=70)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, default=None, blank=True, null=True)
     file_path = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
