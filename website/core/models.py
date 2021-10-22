8b2ac5dfb447b13b85be17f3c3c5c6088649b559
Maxim Shurkhovetskiy <smu580dd@gmail.com>
2021-10-22 23:51:59 +0300

    Change picture model to use ImageField

--- website/core/models.py
+++ website/core/models.py
@@ -12,8 +12,9 @@ class Category(models.Model):
 class Picture(models.Model):
     title = models.CharField(max_length=70)
     description = models.CharField(max_length=200, default=None, blank=True, null=True)
-    file_path = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
