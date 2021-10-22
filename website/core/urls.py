1de10585c64d97fad2b77514c0e26cc224b2c029
Maxim Shurkhovetskiy <smu580dd@gmail.com>
2021-10-22 16:56:53 +0300

    Add templates for index and category pages

--- website/core/urls.py
+++ website/core/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='About'),
-    path('contacts/', views.contacts, name='Contacts'),
-    path('category/<str:category_name>/', views.category, name='Category'),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('category/<str:category_name>/', views.category, name='category'),
 ]
